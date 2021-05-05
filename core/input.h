d92fa9da3e2288146c7fa5e27d5aa6cda69b60a9
Jelle Vos <jelle.vos@outlook.com>
2021-05-05 20:05:18 +0200

    Added mouse input

--- core/input.h
+++ core/input.h
@@ -8,9 +8,17 @@ namespace input
 	{
 		void update();
 		void keyEvent(const SDL_KeyboardEvent* event);
+		void mouseButtonEvent(const SDL_MouseButtonEvent* event);
+		void mouseScrollWheelEvent(const SDL_MouseWheelEvent* event);
 	}
 
 	bool getKey(const int keycode);
 	bool getKeyDown(const int keycode);
 	bool getKeyUp(const int keycode);
+
+	bool getMouseButton(const int button);
+	bool getMouseButtonDown(const int button);
+	bool getMouseButtonUp(const int button);
+
+	short getMouseScrollWheel();
 }
