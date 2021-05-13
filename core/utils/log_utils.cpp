e542c98dd99451a2776549eda59607ce7ceb9bcc
Shantanu Balse <dontstopbelieving@outlook.com>
2021-05-13 14:13:28 +0530

    Log timestamps in log_utils

--- core/utils/log_utils.cpp
+++ core/utils/log_utils.cpp
@@ -2,23 +2,35 @@
 #include "log_utils.h"
 
 #include <iostream>
-#include <time.h>
+#include <ctime>
 #include <sstream>
+#include <iomanip>
 
 namespace log_utils
 {
+	std::string timestamp()
+	{
+		std::stringstream resultStream;
+		std::time_t rawTime = std::time(nullptr);
+		std::tm *localTime = std::localtime(&rawTime);
+
+		resultStream << std::put_time(localTime, "%T");
+
+		return resultStream.str();
+	}
+
 	void info(const char* message)
 	{
-		std::cout << "[INFO] " << message << "\n";
+		std::cout << "[" << timestamp() << " INFO] " << message << "\n";
 	}
 
 	void success(const char* message)
 	{
-		std::cout << "\u001b[32m[SUCCESS] " << message << "\n\u001b[0m";
+		std::cout << "\u001b[32m" << "[" << timestamp() << " SUCCESS] " << message << "\n\u001b[0m";
 	}
 
 	void error(const char* message)
 	{
-		std::cerr << "\u001b[31m[ERROR] " << message << "\n\u001b[0m";
+		std::cerr << "\u001b[31m" << "[" << timestamp() << " ERROR] " << message << "\n\u001b[0m";
 	}
 }
