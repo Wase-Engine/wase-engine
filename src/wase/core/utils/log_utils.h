51a309be537e64b1e78f0ecd62c6aed9ac1e7d0d
Rajeck Massa <rajeckm@hotmail.com>
2021-07-14 23:15:17 +0200

    Update log_utils.h
    Changed string to template

--- src/wase/core/utils/log_utils.h
+++ src/wase/core/utils/log_utils.h
@@ -2,10 +2,31 @@
 
 #include <string>
 
+template <typename typeOfLog>
 namespace log_utils
 {
 	std::string timestamp();
-	void info(const std::string& message);
-	void success(const std::string& message);
-	void error(const std::string& message);
+	
+	
+	void info(const typeOfLog& message);
+	void success(const typeOfLog& message);
+	void error(const typeOfLog& message);
+}
+
+template <typename typeOfLog>
+void info(const typeOfLog& message)
+{
+	std::cout << "[" << timestamp() << " INFO] " << message << "\n";
+}
+
+template <typename typeOfLog>
+void success(const typeOfLog& message)
+{
+	std::cout << "\u001b[32m" << "[" << timestamp() << " SUCCESS] " << message << "\n\u001b[0m";
+}
+
+template <typename typeOfLog>
+void error(const typeOfLog& message)
+{
+	std::cerr << "\u001b[31m" << "[" << timestamp() << " ERROR] " << message << "\n\u001b[0m";
 }
